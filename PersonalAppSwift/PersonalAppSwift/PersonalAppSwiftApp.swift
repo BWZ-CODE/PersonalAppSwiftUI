d61a3051a8dd02c7985de3d283a750874f78b64f
BWZ-CODE <mmige24@gmail.com>
2024-10-15 17:21:22 -0500

    app

---
+++ PersonalAppSwift/PersonalAppSwift/PersonalAppSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalAppSwiftApp.swift
+//  PersonalAppSwift
+//
+//  Created by Miguel Angel Bohorquez on 15/10/24.
+//
+
+import SwiftUI
+
+@main
+struct PersonalAppSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PersonalAppSwift/PersonalAppSwiftTests/PersonalAppSwiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalAppSwiftTests.swift
+//  PersonalAppSwiftTests
+//
+//  Created by Miguel Angel Bohorquez on 15/10/24.
+//
+
+import Testing
+@testable import PersonalAppSwift
+
+struct PersonalAppSwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
